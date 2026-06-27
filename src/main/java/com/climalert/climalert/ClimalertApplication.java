bf064f37fe3bce487f3808dac54c138268a6bb93
Lucio Mazzini <luciomazzini082@gmail.com>
2026-06-27 15:46:30 -0300

    Initial commit: proyecto Spring Boot Climalert

---
+++ src/main/java/com/climalert/climalert/ClimalertApplication.java
@@ -0,0 +1,13 @@
+package com.climalert.climalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/climalert/climalert/ClimalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climalert.climalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
